cee6e381a7e3865d1f287bed5f3b91ce0132a651
Wale Olaleye <2972971+waleo@users.noreply.github.com>
2022-09-29 15:52:49 -0400

    add show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :company_name, :alias)
+  end
 end
