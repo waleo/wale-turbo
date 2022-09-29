f1f80a509c443de066d5dc3c229fc320a4a00c1d
Wale Olaleye <2972971+waleo@users.noreply.github.com>
2022-09-29 17:15:26 -0400

    create update delete actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,35 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    @user.destroy
+    redirect_to users_path, notice: 'User was successfully deleted.'
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :company_name, :alias)
   end
